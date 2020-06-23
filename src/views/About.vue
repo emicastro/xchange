ddb1b2b0fa02247cd93d4422e6c05b9cac8262f4
Emilio Castro <emilio.castro@mercadolibre.com>
2020-06-23 19:32:40 -0300

    Adding filters to format displayed crypto data in a more user friendly way

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,8 @@
   <div class="flex-col items-center">
     <h1 class="text-gray-700 text-6xl">xChange v1.0.0</h1>
     <p class="text-gray-600 text-xl text-center">
-      This app display the value of the most important cryptocurrencies through
-      the Coincap API REST.
+      xChange is a SPA which displays the value of the most important
+      cryptocurrencies through the Coincap REST API.
     </p>
     <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline">
       Back to Home page
